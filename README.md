2a9209fb088275a2289ab1187bd36feab810d618
furio <francesco@furio.me>
2018-03-11 18:11:02 +0100

    m-to-t

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ cups/ => cups stuff + samsung drivers
 plex/ => linuxbase.io plex + intel gpu stuff
 
 artful-base/ => linuxbase.io base image with artful from scratch
+
+magnet-to-transmission/ => a simple script to watch magnet and send to transmission
+
---
+++ magnet-to-transmission/start.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# TR_OPTIONS="192.168.100.3 -n admn:psswrd --downlimit 500 --uplimit 50"
+
+function intfunc
+{
+	echo "`basename $0`: Ouch! User Aborted."
+	exit 1
+}
+
+trap intfunc 2 3 6 15
+
+if [ -z "$TR_IP" ]
+then
+	exit 1
+fi
+
+if [ -z "$TR_SLEEP" ]
+then
+	TR_SLEEP=1m
+fi
+
+if [ -z "$TR_DIR" ]
+then
+	TR_DIR=/watchdir
+fi
+
+while true
+do
+	for MAGNET in $TR_DIR/*.magnet
+	do
+		echo "Parsing file $MAGNET"
+		MGNLINK=$(<"$MAGNET")
+		transmission-remote $TR_IP $TR_OPTIONS --add $MGNLINK
+		# echo $MGNLINK
+		if [ $? -eq 0 ]
+		then
+			mv "$MAGNET" "$MAGNET.added"
+		fi
+	done
+
+	sleep $TR_SLEEP
+done
+
+exit 0
+
+
